234ef53596056afa915bd6d98e0c8cca4c12cc77
Nakul <nakul.kumar627@gmail.com>
2025-03-19 16:32:48 +0000

    Win,draw,loss refactored into GameUtils class

--- src/app.ts
+++ src/app.ts
@@ -60,25 +60,25 @@ io.on("connection", function (socket) {
 
     // check if the received message contains 'grid' or not
     // check if move is valid or not - continous moves, wrong symbol used, overwriting
-    // if(!checkValidity(message)) {
-    //   socket.emit('game',"Invalid Move");
-    //   socket.disconnect();
-    // }
+    if(!GameUtils.checkValidity(message)) {
+      socket.emit('game',"Invalid Move");
+      socket.disconnect();
+    }
 
     // if valid, update the grid on the server
     serverGrid = message;
 
 
     // check for win, loss
-    let aiWin = checkWin(serverGrid,'X');
-    let userWin = checkWin(serverGrid, 'O');
+    let aiWin = GameUtils.checkWin(serverGrid,'X');
+    let userWin = GameUtils.checkWin(serverGrid, 'O');
     if((aiWin && !userWin) || (!aiWin && userWin) ) {
       socket.emit('game', "GAME OVER");
       socket.disconnect();
     }
 
     // check for draw
-    if (checkDraw(serverGrid)) {
+    if (GameUtils.checkDraw(serverGrid)) {
       socket.emit('game', "DRAW");
       socket.disconnect();
     }
@@ -91,8 +91,8 @@ io.on("connection", function (socket) {
     serverGrid[aiMove.row][aiMove.col] = 'X';
 
     // check for win, loss
-    aiWin = checkWin(serverGrid,'X');
-    userWin = checkWin(serverGrid, 'O');
+    aiWin = GameUtils.checkWin(serverGrid,'X');
+    userWin = GameUtils.checkWin(serverGrid, 'O');
     if((aiWin && !userWin) || (!aiWin && userWin) ) {
       socket.emit('game', "GAME OVER");
       socket.emit('game', serverGrid); // return the grid to the user
@@ -100,7 +100,7 @@ io.on("connection", function (socket) {
     }
 
     // check for draw
-    if (checkDraw(serverGrid)) {
+    if (GameUtils.checkDraw(serverGrid)) {
       socket.emit('game', "DRAW");
       socket.emit('game', serverGrid); // return the grid to the user
       socket.disconnect();
@@ -111,128 +111,131 @@ io.on("connection", function (socket) {
 
 });
 
-function checkValidity(userGrid : string[][]) : boolean {
-  // validate grid, means check if the move is valid or not
 
-  // two inputs - one grid from user and one grid inside the server
 
-  // disparity > 1 then also problem
-  // compare the two inputs to check for overwriting
-  // this function will run everytime the user makes a move before the whole recursion process of the AI
-  // disparity should not be zero or greater than 1
-  // the new move should overwrite a dash
-
+class GameUtils {
+  public static checkValidity(userGrid : string[][]) : boolean {
+    // validate grid, means check if the move is valid or not
   
-  // TO DO - if the input sent by the user is an actual grid or not
-
-
-  // user should not place the symbol assigned to the AI
-
-  let disparity = 0;
-  for(let i = 0; i < userGrid.length; i++) {
-    for(let j = 0; j < userGrid[0].length; j++) {
-
-      if (userGrid[i][j] != serverGrid[i][j] && serverGrid[i][j] == '-') {
-        // user has made a move
-        if (userGrid[i][j] == 'X') return false;
-        disparity++;
-        // if(disparity > 1 || disparity == 0) {
-        // case of making two moves at once
-        // disparity = 0 means no move has been made by the player
-        // socket.disconnect
-        // }
-      }
-      if (userGrid[i][j] != serverGrid[i][j] && serverGrid[i][j] != '-') {
-        // case of overwriting
-        return false;
-        // socket.disconnect
-      }
-    }
-  }
-  if (disparity == 1) return true;
-  else return false;
-}
-
-// returns true if game is draw
-function checkDraw(grid: string[][]): boolean {
-  // check for draw
-  let draw : boolean = true;
-  for(let i = 0; i < grid.length; i++) {
-    for(let j = 0; j < grid[0].length; j++) {
-      if(grid[i][j] == '-') {
-        draw = false;
-        break;
-      }
-    }
-    if(!draw) return false;
-  }
-  if(draw) return true;
-  return false;
-}
-
-
-// checks if the AI has won or not
-function checkWin(grid : string[][], checkMark: string) : boolean {
-
+    // two inputs - one grid from user and one grid inside the server
   
-  // 0 is draw
-  // 1 is win
-  // 2 is loss
-  // 3 is continue
-
-  // horizontal
-  for(let i = 0; i < grid.length; i++) {
-    let win : boolean = true;
-    for(let j = 0; j < grid[0].length; j++) {
-      if(grid[i][j] != checkMark) {
-        win = false;
-        break;
+    // disparity > 1 then also problem
+    // compare the two inputs to check for overwriting
+    // this function will run everytime the user makes a move before the whole recursion process of the AI
+    // disparity should not be zero or greater than 1
+    // the new move should overwrite a dash
+  
+    
+    // TO DO - if the input sent by the user is an actual grid or not
+  
+  
+    // user should not place the symbol assigned to the AI
+  
+    let disparity = 0;
+    for(let i = 0; i < userGrid.length; i++) {
+      for(let j = 0; j < userGrid[0].length; j++) {
+  
+        if (userGrid[i][j] != serverGrid[i][j] && serverGrid[i][j] == '-') {
+          // user has made a move
+          if (userGrid[i][j] == 'X') return false;
+          disparity++;
+          // if(disparity > 1 || disparity == 0) {
+          // case of making two moves at once
+          // disparity = 0 means no move has been made by the player
+          // socket.disconnect
+          // }
+        }
+        if (userGrid[i][j] != serverGrid[i][j] && serverGrid[i][j] != '-') {
+          // case of overwriting
+          return false;
+          // socket.disconnect
+        }
       }
     }
-    if(win) return true;
+    if (disparity == 1) return true;
+    else return false;
   }
 
-  // vertical
-  for(let j = 0; j < grid[0].length; j++) {
-    let win : boolean = true;
+  // returns true if game is draw
+  public static checkDraw(grid: string[][]): boolean {
+    // check for draw
+    let draw : boolean = true;
     for(let i = 0; i < grid.length; i++) {
+      for(let j = 0; j < grid[0].length; j++) {
+        if(grid[i][j] == '-') {
+          draw = false;
+          break;
+        }
+      }
+      if(!draw) return false;
+    }
+    if(draw) return true;
+    return false;
+  }
+
+  // checks if the AI has won or not
+  public static checkWin(grid : string[][], checkMark: string) : boolean {
+  
+    
+    // 0 is draw
+    // 1 is win
+    // 2 is loss
+    // 3 is continue
+  
+    // horizontal
+    for(let i = 0; i < grid.length; i++) {
+      let win : boolean = true;
+      for(let j = 0; j < grid[0].length; j++) {
+        if(grid[i][j] != checkMark) {
+          win = false;
+          break;
+        }
+      }
+      if(win) return true;
+    }
+  
+    // vertical
+    for(let j = 0; j < grid[0].length; j++) {
+      let win : boolean = true;
+      for(let i = 0; i < grid.length; i++) {
+        if(grid[i][j] != checkMark) {
+          win = false;
+          break;
+        }
+      }
+      if(win) return true;
+    }
+  
+    // diagonal
+    let i = 0;
+    let j = 0;
+    let diagonal : boolean = true;
+    while(i < grid.length && j < grid.length) {
       if(grid[i][j] != checkMark) {
-        win = false;
+        diagonal = false;
         break;
       }
+      i++;
+      j++;
     }
-    if(win) return true;
-  }
-
-  // diagonal
-  let i = 0;
-  let j = 0;
-  let diagonal : boolean = true;
-  while(i < grid.length && j < grid.length) {
-    if(grid[i][j] != checkMark) {
-      diagonal = false;
-      break;
-    }
-    i++;
-    j++;
-  }
-  if(diagonal) return true;
-
-  i = 0;
-  j = grid.length-1;
-  diagonal = true;
-  while(i < grid.length && j >= 0) {
-    if(grid[i][j] != checkMark) {
-      diagonal = false;
-      break;
-    }
-    i++;
-    j--;
-  }
-  if(diagonal) return true;
+    if(diagonal) return true;
   
-
-  return false;
+    i = 0;
+    j = grid.length-1;
+    diagonal = true;
+    while(i < grid.length && j >= 0) {
+      if(grid[i][j] != checkMark) {
+        diagonal = false;
+        break;
+      }
+      i++;
+      j--;
+    }
+    if(diagonal) return true;
+    
+  
+    return false;
+  }
 }
 
 class AiUtils {
@@ -251,15 +254,15 @@ class AiUtils {
         value = min(value, AImove(possibleState, player * -1))
   */
   console.log('player: ', player);
-    if (checkWin(grid, 'X')) {
+    if (GameUtils.checkWin(grid, 'X')) {
       console.log('AI Wins', grid);
       return 1;
     }
-    if (checkWin(grid, 'O')) {
+    if (GameUtils.checkWin(grid, 'O')) {
       console.log('User wins', grid);
       return -1;
     }
-    if (checkDraw(grid)) {
+    if (GameUtils.checkDraw(grid)) {
       console.log('draw', grid);
       return 0;
     }
